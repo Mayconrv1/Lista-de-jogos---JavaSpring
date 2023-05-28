d5d9ca7c715705d4fe72f62515ed57deba1fa52c
Maycon Renan <mayconrenan175@gmail.com>
2023-05-28 02:28:40 -0300

    GET game by id

--- src/main/java/com/example/demo/controllers/GameController.java
+++ src/main/java/com/example/demo/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.GameDTO;
 import com.example.demo.dto.GameMinDTO;
 import com.example.demo.services.GameService;
 
@@ -16,6 +18,12 @@ public class GameController {
 	
 	@Autowired
 	private GameService gameService;
+	
+	@GetMapping(value = "/{id}")
+	public GameDTO findByid(@PathVariable Long id){
+		GameDTO result = gameService.findById(id);
+		return result;
+	}
 
 	@GetMapping
 	public List<GameMinDTO> findAll(){
--- src/main/java/com/example/demo/services/GameService.java
+++ src/main/java/com/example/demo/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.demo.dto.GameDTO;
 import com.example.demo.dto.GameMinDTO;
 import com.example.demo.entities.Game;
 import com.example.demo.repositories.GameRepository;
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		 GameDTO dto = new GameDTO(result);
+		 return dto;
+	}
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> finAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto= result.stream().map(x -> new GameMinDTO(x)).toList();
